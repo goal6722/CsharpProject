6096b757c882afd4b55434dde1e66b0821996722
909ma <sin00021@naver.com>
2023-07-17 12:17:51 +0900

    add DB MyProduct

---
+++ 소스 파일/김신혁 - MyProduct 테이블/AddDB/AddDB/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            string basePath = "C:\\Users\\sin00\\OneDrive\\문서\\GitHub\\c++++Project\\xml파일\\"; // xml 파일 경로
+            string fileExtension = ".xml";
+
+            for (int i = 1; i <= 9886; i++)
+            {
+                string fileName = i.ToString("D5") + fileExtension;
+                string xmlFilePath = basePath + fileName;
+
+                XmlToDbHelper.InsertXmlDataToDb(xmlFilePath);
+
+                Console.WriteLine(i);
+            }
+        }
+
+
+    }
+}
