9d721637e2af6b5339092df44ebb4794b39cc2a4
909ma <sin00021@naver.com>
2023-07-24 10:23:59 +0900

    통합 메인 화면

---
+++ 소스 파일/김신혁 - 통합 메인 화면/Main/Main/Form1.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace Main
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.DataBase = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBoxLarge = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.SearchProductButton1 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.DataBase.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1801, 962);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 28);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1793, 930);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.DataBase);
+            this.tabPage2.Controls.Add(this.groupBox2);
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 28);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1793, 930);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "통계";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // DataBase
+            // 
+            this.DataBase.Controls.Add(this.label5);
+            this.DataBase.Controls.Add(this.label4);
+            this.DataBase.Location = new System.Drawing.Point(16, 14);
+            this.DataBase.Name = "DataBase";
+            this.DataBase.Size = new System.Drawing.Size(285, 44);
+            this.DataBase.TabIndex = 15;
+            this.DataBase.TabStop = false;
+            this.DataBase.Text = "DataBase";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(69, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 18);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "^^";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 18);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "개수";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chart1);
+            this.groupBox2.Location = new System.Drawing.Point(14, 67);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1764, 686);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "차트 영역";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.comboBoxLarge);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(14, 759);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(428, 158);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "필터";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(304, 124);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 26);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "검색";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(304, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 26);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "검색";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(458, 53);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 26);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "검색";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(74, 124);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(214, 26);
+            this.comboBox3.TabIndex = 7;
+            // 
+            // comboBoxLarge
+            // 
+            this.comboBoxLarge.FormattingEnabled = true;
+            this.comboBoxLarge.Location = new System.Drawing.Point(74, 35);
+            this.comboBoxLarge.Name = "comboBoxLarge";
+            this.comboBoxLarge.Size = new System.Drawing.Size(214, 26);
+            this.comboBoxLarge.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "소분류";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "대분류";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 18);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "중분류";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(74, 79);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(214, 26);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.SearchProductButton1);
+            this.tabPage3.Controls.Add(this.textBox1);
+            this.tabPage3.Controls.Add(this.label1);
+            this.tabPage3.Controls.Add(this.pictureBox1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 28);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1793, 930);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "상품 검색";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1086, 689);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(662, 28);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1083, 624);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "상품명";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1064, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(701, 580);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SearchProductButton1
+            // 
+            this.SearchProductButton1.Location = new System.Drawing.Point(1100, 765);
+            this.SearchProductButton1.Name = "SearchProductButton1";
+            this.SearchProductButton1.Size = new System.Drawing.Size(633, 127);
+            this.SearchProductButton1.TabIndex = 3;
+            this.SearchProductButton1.Text = "상품 검색";
+            this.SearchProductButton1.UseVisualStyleBackColor = true;
+            this.SearchProductButton1.Click += new System.EventHandler(this.SearchProductButton1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(304, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 26);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "검색";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(0, 27);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(1768, 659);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1798, 959);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.DataBase.ResumeLayout(false);
+            this.DataBase.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox DataBase;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBoxLarge;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button SearchProductButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ 소스 파일/김신혁 - 통합 메인 화면/Main/Main/Form1.cs
@@ -0,0 +1,130 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Main
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            CreateComboBoxListL();
+            label5.Text = DBHelper.CountDB();
+            Dictionary<string, int> divLCounts = DBHelper.GetDivLCounts();
+
+            chart1.ChartAreas[0].AxisX.Interval = 1;
+            chart1.Series[0].Name = "시장품목";
+
+            foreach (var kvp in divLCounts)
+            {
+                string divLValue = kvp.Key;
+                int count = kvp.Value;
+                chart1.Series[0].Points.AddXY(divLValue, count);
+            }
+        }
+
+        public void SearchProductButton1_Click(object sender, EventArgs e)//상품검색
+        {
+            string jsonFilePath = @"log.json";
+            string jsonContent = File.ReadAllText(jsonFilePath);
+            List<ImageData> imageDataList = JsonConvert.DeserializeObject<List<ImageData>>(jsonContent);
+            string userInput = textBox1.Text;
+            string matchedFileName = imageDataList.Find(x => x.Id == userInput)?.FileName;
+
+            if (matchedFileName != null)
+            {
+                label1.Text = matchedFileName;
+                string imagePath = @"Resource\" + matchedFileName;
+                string imageFilePath = System.IO.Path.Combine(Application.StartupPath, imagePath);
+                pictureBox1.Image = Image.FromFile(imageFilePath);
+            }
+            else
+            {
+                label1.Text = "일치하는 파일을 찾을 수 없습니다.";
+                string imagePath = @"Resource\null.jpg";
+                string imageFilePath = System.IO.Path.Combine(Application.StartupPath, imagePath);
+                pictureBox1.Image = Image.FromFile(imageFilePath);
+            }
+        }
+
+        private void CreateComboBoxListL()
+        {
+            comboBoxLarge.Items.Add("--대분류--");
+            List<string> divName = DBHelper.DivListL("div_l");
+            foreach (var div in divName)
+            {
+                string comboName = div;
+                comboBoxLarge.Items.Add(comboName);
+            }
+            comboBoxLarge.SelectedIndex = 0;
+        }
+
+        private void CreateComboBoxListM(string constName)
+        {
+            comboBox1.Items.Clear();
+            comboBox1.Items.Add("--중분류--");
+            List<string> divName = DBHelper.DivListMnS("div_m", "div_l", constName);
+            foreach (var div in divName)
+            {
+                string comboName = div;
+                comboBox1.Items.Add(comboName);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+        private void CreateComboBoxListS(string constName)
+        {
+            comboBox3.Items.Clear();
+            comboBox3.Items.Add("--소분류--");
+            List<string> divName = DBHelper.DivListMnS("div_s", "div_m", constName);
+            foreach (var div in divName)
+            {
+                string comboName = div;
+                comboBox3.Items.Add(comboName);
+            }
+            comboBox3.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            string selectedDiv = comboBoxLarge.SelectedItem.ToString();
+            string columnName = '\'' + comboBoxLarge.Text + '\'';
+            Dictionary<string, int> filteredData = DBHelper.GetFilteredData(columnName, "div_l", "div_m");
+            UpdateChart(filteredData);
+            CreateComboBoxListM(columnName);
+        }
+
+        private void UpdateChart(Dictionary<string, int> data)
+        {
+            chart1.Series[0].Points.Clear();
+            foreach (var kvp in data)
+            {
+                string divValue = kvp.Key;
+                int count = kvp.Value;
+                chart1.Series[0].Points.AddXY(divValue, count);
+            }
+        }
+
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            string selectedDiv = comboBox1.SelectedItem.ToString();
+            string columnName = '\'' + comboBox1.Text + '\'';
+            Dictionary<string, int> filteredData = DBHelper.GetFilteredData(columnName, "div_m", "div_s");
+            UpdateChart(filteredData);
+            CreateComboBoxListS(columnName);
+        }
+
+        private void button3_Click(object sender, System.EventArgs e)
+        {
+            string selectedDiv = comboBox3.SelectedItem.ToString();
+            string columnName = '\'' + comboBox3.Text + '\'';
+            Dictionary<string, int> filteredData = DBHelper.GetFilteredData(columnName, "div_s", "img_prod_nm");
+            UpdateChart(filteredData);
+        }
+    }
+}
