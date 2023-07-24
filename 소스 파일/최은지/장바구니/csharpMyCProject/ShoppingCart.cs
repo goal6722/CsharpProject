afecbdee6a9988cca86a67c0026203f31f51f9ee
EunjiCh0i <cyj01105@naver.com>
2023-07-24 13:44:31 +0900

    장바구니완료
    영수증 텍스트파일 추가 완료

---
+++ 소스 파일/최은지/장바구니/csharpMyCProject/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpMyCProject
+{
+	public class ShoppingCart
+	{
+		//품목
+		public string item { get; set; }
+
+		//개수
+		public int count { get; set; }
+
+		//금액
+		public int pay{ get; set; } 
+
+		//바코드
+		public string barcode { get; set; }
+	}
+}
