8b09725f6ebdaae4531261f613d3db744c2fd378
EunjiCh0i <cyj01105@naver.com>
2023-07-24 16:37:00 +0900

    장바구니통합중
    소스통합중

--- 소스 파일/이수성 - 통합 및 개인 소스/Main이수성/Main/DBHelperBarcode.cs
+++ 소스 파일/이수성 - 통합 및 개인 소스/Main이수성/Main/DBHelperBarcode.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Main
 {
-	internal class DBHelperBarcode
+	public class DBHelperBarcode
 	{
+		private SqlConnection conn;
+
+		public DBHelperBarcode()
+		{
+			// 데이터베이스 연결 문자열 초기화
+			string dataSource = "909MA";//KB-PC
+			string db = "MYDB";
+			string security = "SSPI";
+			string connectionString = $"Data Source={dataSource}; Initial Catalog={db};" +
+				$"Integrated Security={security}; Timeout=30";
+			conn = new SqlConnection(connectionString); // SqlConnection 객체를 생성하여 연결 문자열을 할당
+		}
+
+		public void OpenConnection()
+		{
+			if (conn.State == System.Data.ConnectionState.Closed)
+			{
+				conn.Open();
+			}
+		}
+
+		public void CloseConnection()
+		{
+			if (conn.State == System.Data.ConnectionState.Open)
+			{
+				conn.Close();
+			}
+		}
+
+		public SqlDataReader ExecuteReader(string query)
+		{
+			SqlCommand command = new SqlCommand(query, conn);
+			return command.ExecuteReader();
+		}
+
+		public int ExecuteNonQuery(string query)
+		{
+			SqlCommand command = new SqlCommand(query, conn);
+			return command.ExecuteNonQuery();
+		}
 	}
 }
+
